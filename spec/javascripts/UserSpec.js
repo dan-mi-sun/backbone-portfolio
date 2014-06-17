32d5b3c65b37790764576b7d845b40d06eec17fe
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-17 16:43:17 +0100

    add user spec test passing

---
+++ spec/javascripts/UserSpec.js
@@ -0,0 +1,31 @@
+describe("A User", function() {
+
+  var user;
+
+  beforeEach(function() {
+    user = new app.models.User({
+      fullName: "Daniel Sun",
+      bio: "Wax on, wax off",
+      imgUrl: "#",
+      mission: "Time travel"
+    });
+  });
+
+  it("should be able to retreive the name", function() {
+    expect(user.get("fullName")).toEqual("Daniel Sun");
+  });
+
+
+  describe("validation", function() {
+    beforeEach(function() {
+      user = new app.models.User({
+        fullName: ""
+      });
+    });
+
+    it("should not be valid without a name", function() {
+      expect(user.isValid()).toBeFalsy();
+    });
+  });
+
+});
