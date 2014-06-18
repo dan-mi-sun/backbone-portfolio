0748ed0b5bef5db2a3638e5d49116848a9bab814
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-18 17:06:40 +0100

    remove defunct code and whitespace

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -17,11 +17,6 @@ app.models.User = Backbone.Model.extend({
   validate: function() {
     if(this.attributes.fullName === "" || this.get('fullName') === undefined) {
       return "Name can't be blank"; 
-    //   var errors = {messages : []};
-    // if(!!(this.get('name'))) {
-    //   errors.messages.push("Please fill in the name");
-    //   return errors;
-    // }
     };
   },
 
