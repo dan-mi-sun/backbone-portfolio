08cf8b709138324ce11e52bd36ce51ce5ddb1817
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-20 11:54:18 +0100

    add rails

--- app/assets/javascripts/views/user_view.js
+++ app/assets/javascripts/views/user_view.js
@@ -13,33 +13,33 @@ app.views.UserView = Backbone.View.extend({
 
   render: function() {
   //Render the user bio section
-  this.$el.html(this.template(this.model.attributes));
+    this.$el.html(this.template(this.model.attributes));
 
-  // Create a dummy project if there isn't one already
-  if(this.model.projects.length == 0) {
-    this.model.projects.add({
-      title: "New Project",
-      url: "Click to edit",
-      body: "Click to edit"
+    // Create a dummy project if there isn't one already
+    if(this.model.projects.length == 0) {
+      this.model.projects.add({
+        title: "New Project",
+        url: "Click to edit",
+        body: "Click to edit"
+      });
+    }
+
+    //Clear old projects from view
+    $('#project-list').html("");
+
+    // Render each project... sort of like a partial...
+    this.model.projects.each(function(project) {
+      var view = new app.views.ProjectView({ model: project });
+      $('#project-list').append(view.render().el);
     });
-  }
 
-  //Clear old projects from view
-  $('#project-list').html("");
-  
-   // Render each project... sort of like a partial...
-  this.model.projects.each(function(project) {
-    var view = new app.views.ProjectView({ model: project });
-    $('#project-list').append(view.render().el);
-  });
-
-   // Redraw the page if the user model changes
+    // Redraw the page if the user model changes
     this.listenTo(this.model, "change", this.render);
 
     return this;
-    },
+  },
 
-  editAttribute function(e) {
+  editAttribute: function(e) {
     $(e.currentTarget).hide().next('.hidden-edit').fadeIn();
   },
 
@@ -57,5 +57,5 @@ app.views.UserView = Backbone.View.extend({
     this.model.set('mission', e.currentTarget.value);
     this.model.save();
   }
-  
+
 });
